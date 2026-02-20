1ed92f545b6fb355ba3ffa3b466244d2746b0472
omkumava <omkumavat2004@gmail.com>
2026-02-20 14:04:51 +0530

    done

--- Backend1/src/main/java/payment/system/SystemApplication.java
+++ Backend1/src/main/java/payment/system/SystemApplication.java
@@ -22,3 +22,5 @@ public class SystemApplication {
 		return "Om";
 	}
 }
+
+
--- Backend1/src/main/java/payment/system/config/FirebaseConfig.java
+++ Backend1/src/main/java/payment/system/config/FirebaseConfig.java
@@ -6,8 +6,10 @@ import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
@@ -15,8 +17,13 @@ public class FirebaseConfig {
     @PostConstruct
     public void init() throws IOException {
 
-        FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/firebase-service-account.json");
+        String firebaseConfig = System.getenv("FIREBASE_CONFIG");
+
+        if (firebaseConfig == null || firebaseConfig.isEmpty()) {
+            throw new IllegalStateException("FIREBASE_CONFIG not set");
+        }
+
+        InputStream serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes(StandardCharsets.UTF_8));
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
