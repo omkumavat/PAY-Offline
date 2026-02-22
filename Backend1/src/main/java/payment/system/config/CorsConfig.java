da9255a79f5b45b3b30126015e7b8e3ceb83a0a5
omkumava <omkumavat2004@gmail.com>
2026-02-22 23:06:06 +0530

    done

--- Backend1/src/main/java/payment/system/config/CorsConfig.java
+++ Backend1/src/main/java/payment/system/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "http://localhost:5173",
+                                "https://pay-offline-qr.vercel.app",
                                 "http://localhost:4173"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
